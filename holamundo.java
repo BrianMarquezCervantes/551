c3541957afbf04720ca8074daab8b835baa59a36
BrianMarquezCervantes <dinki34567@gmail.com>
2023-05-18 16:22:06 -0600

    se ingreso holamundo

---
+++ holamundo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.holaholahola;
+
+/**
+ *
+ * @author Dynky
+ */
+public class holiiiii {
+    public static void main (String[]args)
+            {
+                System.out.println("Hola mundo las estrellas les dicen hola");
+            }
+}
+    
