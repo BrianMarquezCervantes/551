8cfb5f3d4ea4dd1c0c5889a0e74c2e1efe778d01
BrianMarquezCervantes <dinki34567@gmail.com>
2023-05-18 16:42:37 -0600

    se agrega V2TRIFIV

---
+++ V2TRIFIV.java
@@ -0,0 +1,40 @@
+
+package com.mycompany.v2trifiv;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class V2TRIFIV {
+  
+    public static void main (String[]args) throws IOException
+    {
+        BufferedReader bufEntrada = new BufferedReader(new InputStreamReader(System.in));
+        int numero,mult3,mult5;
+        System.out.println("Dame el numero:");
+        numero= Integer.parseInt(bufEntrada.readLine());
+        
+        mult3= numero%3;
+        mult5=numero%5;
+        
+       if(mult5==0 && mult3 ==0)
+       {
+           System.out.println("TRIFIVE");
+           
+       }
+       else if(mult5==0)
+       {
+           System.out.println("FIVE");
+       }
+       else if(mult3 ==0)
+       {
+           System.out.println("TRI");
+       }
+                
+        
+    }
+}
+
+    
+
